5a4533e99575517b4643bdd685ce7ca325d8e862
Pavna Yejare <pavan.yejare@gmail.com>
2020-06-02 21:04:47 +0530

    first cut of flask on elastick beanstalk

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # AWS_EB_python_flask
+Setup to application on AWS elastic beanstalk use the following setps
+
+1. install aws cli and configure it 
+2. install awseb cli to setup the code using cicd or post git commit
+3. confiure .ebextensions/  and .elasticbeanstalk/ as per  requirement 
+4. run following command to deploy application 
+    eb init 
+    eb create APP_NAME
+    eb deploy
---
+++ application.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+from flask import Flask, render_template,request, url_for
+import os
+import datetime
+
+application = app = Flask(__name__)
+@app.route("/")
+def new_regi():
+	return render_template('registration.html')
+@app.route('/scan',methods=['POST'])
+def scan():
+    id= request.form['id']
+    name = request.form['name']
+    data = {'id' : id, 'name' : name}
+    data1 = { 0 : id, 1 : name}
+    #db.store(data1)
+    return render_template('registration.html')
+
+
+
+if __name__ == "__main__":
+     app.run(host="0.0.0.0",port=80, debug=True)
+
